df277ff218f47e50382de2baee8b4435b28b3c7f
Nik <nwakg@pm.me>
2023-09-10 19:11:35 -0700

    Add Add SCREAMING_SNAKE_CASE

---
+++ src/strings/to_screaming_snake_case.rs
@@ -0,0 +1,42 @@
+// Converts an input to SCREAMING_SNAKE_CASE. Does not filter out any special characters.
+pub fn to_screaming_snake_case(input: &str) -> String {
+    let chars = input.chars();
+    let mut output = String::new();
+    let mut prev: Option<char> = None;
+    for char in chars {
+        if char.is_uppercase() && prev.map(|prev_char| prev_char.is_lowercase()).unwrap_or(false) {
+            output.push('_');
+        }
+        if char.is_lowercase() {
+            for char in char.to_uppercase() {
+                output.push(char);
+            }
+        } else {
+            output.push(char);
+        }
+        prev.replace(char);
+    }
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::to_screaming_snake_case;
+
+    macro_rules! test_case {
+        ($input:expr, $expected:expr) => {
+            assert_eq!(to_screaming_snake_case($input), $expected)
+        };
+    }
+    #[test]
+    fn to_camel_case_converts() {
+        test_case!("FoodTruck", "FOOD_TRUCK");
+        test_case!("foodTruck", "FOOD_TRUCK");
+        test_case!("FOodTRUck", "FOOD_TRUCK");
+        test_case!("foodTruck", "FOOD_TRUCK");
+        test_case!("FOOD_TRUCK", "FOOD_TRUCK");
+        test_case!("foodtruck", "FOODTRUCK");
+        test_case!("_food_truck", "_FOOD_TRUCK");
+        test_case!("_FOOD_TRUCK", "_FOOD_TRUCK");
+    }
+}
