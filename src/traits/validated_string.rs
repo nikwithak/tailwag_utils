3c48914543fefa5bda594a9c15c5eb19a68ae70e
Nik <65584296+nikwithak@users.noreply.github.com>
2023-10-18 14:20:25 -0700

    Add validated_string docs

--- src/traits/validated_string.rs
+++ src/traits/validated_string.rs
@@ -1,10 +1,25 @@
 use std::{fmt::Debug, marker::PhantomData, ops::Deref, sync::Arc};
 
+/// Defines validation rules for a `&str`.
+///
+/// Implement this trait to created a new `ValidatedConstantString` type, which
+/// enforces the rules defined in `ValidateString::validate`. See [ValidatedConstantString]
+/// for more detail.
 pub trait ValidateString {
     type Error: Debug;
     fn validate(value: &str) -> Result<(), Self::Error>;
 }
 
+/// An immutable string type, which enforces the string structure
+/// via the [ValidateString] implementation `V`.
+///
+/// Use this struct to enforce rules on identifiers, where they must follow
+/// a specific pattern.
+///
+/// The inline macro [create_validated_string_type] can be used to quickly
+/// create new types with enforced validation rules based on a regular expression.
+/// ```
+#[derive(Clone)]
 pub struct ValidatedConstantString<V>
 where
     V: ValidateString,
@@ -42,6 +57,15 @@ where
     }
 }
 
+/// Creates a ValidatedConstantString for the given regex pattern.
+///
+/// IMPORTANT NOTE: Due to constraints around macro_rules!, you will encounter namespace
+/// collissions if you use this multiple times within the same scope. To get around this
+/// restriction you may need to do
+///
+/// Examples:
+///
+/// TODO
 #[macro_export]
 macro_rules! create_validated_string_type {
     ($validator_name:ident=$regex:tt) => {
